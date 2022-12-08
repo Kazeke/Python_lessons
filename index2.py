6b8405ad9388c4cdf3d01cf0e38237230d9bdc57
Kazeke <kazbek_kadirov@mail.ru>
2022-12-08 21:47:05 +0500

    lesson

---
+++ index2.py
@@ -0,0 +1,6 @@
+a = 4
+b = 2
+if a % 2 == 0 and b % 2 == 0:
+    print('Ok')
+else:
+    print('Ne oK')
---
+++ index3.py
@@ -0,0 +1,3 @@
+pass1 = input('password')
+if len(pass1)>=8 and (('@')in pass1) or (('#') in pass1):
+    print('ok')
