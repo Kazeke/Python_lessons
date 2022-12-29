1f162f8e5b696b11262b7f96980f10e9e250c7c9
Kazeke <kazbek_kadirov@mail.ru>
2022-12-29 21:31:23 +0500

    D8

---
+++ D8/1.py
@@ -0,0 +1,19 @@
+def a(x,y):
+    return x+y
+
+a(12,3)
+
+''' это комментарий '''
+# это тоже комментарий
+
+def create_image(name: str, path_file: str, frame_count: int)->str:
+    """ """
+
+class CustomeClass:
+    ''''''
+
+def custome_sum(some_iter, some_iter2):
+    pass
+
+custome_sum('11', ' wefdw')
+
---
+++ D8/2.py
@@ -0,0 +1,5 @@
+def sum_one_two(first_number, second_number):
+    summa = (first_number + second_number)**2
+    return summa
+    
+print(sum_one_two(10, 20))
